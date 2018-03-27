2509fa3b727de2dd256a045db556dd285c7f0271
yqh123 <1261451680@qq.com>
2018-03-27 14:14:02 +0800

    iii

--- card.js
+++ card.js
@@ -49,7 +49,7 @@ var config = {
                 }
             },
             error: function(err) {
-                alert(err);
+                alert(JSON.stringify(err));
             }
         });
     },

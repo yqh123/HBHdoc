2f1c8e37a8bd390395520a003f0ac3a4da84d40b
yqh123 <1261451680@qq.com>
2018-03-27 14:16:53 +0800

    jjj

--- card.js
+++ card.js
@@ -49,7 +49,7 @@ var config = {
                 }
             },
             error: function(err) {
-                alert(JSON.stringify(err));
+                alert('网络错误')
             }
         });
     },
